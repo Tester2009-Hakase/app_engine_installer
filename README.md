7f1bd897f1ecccbbe4c9a65d1ae844fa026cc996
Мухаммад Алифф Муаззам <sulakecorporation1232@gmail.com>
2017-06-30 16:03:54 +0800

    some worst translation done here

--- README.md
+++ README.md
@@ -1,13 +1,10 @@
 # app_engine_installer
-Telegramda bo't tuzib, google apps engine ga bo'tini o'rnatmoxchilar uchun script.
+A script for scriptwriting and google apps engine.
 
-bo't tuzishni o'rgatadigan scriptni o'rnatish va yoqish uchun.
+To set up and enable scripting that will help you create bot.
 
-```git clone https://github.com/Oybekuz/app_engine_installer.git```
+```git clone git@github.com:Tester2009-Hakase/app_engine_installer.git```
 
 ```python app_engine_installer/main.py```
 
-https://github.com/eternnoir/pyTelegramBotAPI ni modifikatsiya qilingan va google apps engine ga tushadigan qilip o'zgartirilgani
-
-Diqqat, script yozan barcha narsasiga yaxshilap e'tibor bering. Google da qilinadigan barcha ishlar google akkantiga kirilgan holda qilinadi
-
+https://github.com/eternnoir/pyTelegramBotAPI has been modified
--- main.py
+++ main.py
@@ -12,40 +12,40 @@ def a():
     data = urllib2.urlopen("https://storage.googleapis.com/appengine-sdks/featured/google_appengine_1.9.38.zip").read()
     open('./master.zip',"w").write(data)
     data = ""
-    
+
 threading.Thread(target=a).start()
 
 def wait(instruction):
     while raw_input("O'xshadimi? y/n:").lower() != "y":
         print(instruction)
 
-print("Salom, bu script sizga google ga bo't joylashga yordam beradi. 1) appengine.google.com ga kiring. Kirganingizda \"y\" dip yozing.")
-while raw_input("Kirdingizmi? y/n:").lower() != "y":
-    print('https://appengine.google.com/ ga kiring. Bu qiyinamas')
+print("Hello, this script will help you on Google. 1) Visit appengine.google.com. Type \"y\" when you sign in.")
+while raw_input("Enter? y/n:").lower() != "y":
+    print('Opening https://appengine.google.com/Bu qiyinamas')
 
-print("Ok. Kirgan bo'lsangiz, u yerda \033[4m'Create project'\033[0m yoki \033[4m'Создать проект'\033[0m tugmasini bosing. Bosgandan keyin yana 'y' ni bosing.")
-wait("Yaxshilap e'tibor bilan qidiring. Script topib beomidi. Topsangiz, 'y' jo'nating")
-print("Menyu ochiladi, va u yerga project nomini yozasiz. Yaxshisi, bo'tingizni userneymini yozing. Misol uchun, @intelekt_bot ni intelekt-bot deb, shunda chalkashishlar kam bo'ladi.")
-print("\033[1m Project nomi bila id si bir xil bo'lgani yaxshi. Project nomi va id sini eslap qoling!\033[0m")
-print('proektni sozdat qilishni bosing. Pasda "creating project..." yoki shunga o\'xshagan yozuv chiqadi')
-wait("Project nomini va id sini eslap qolip, create yoki \033[95mсоздать\033[0m ni \033[95mbosing\033[0m")
-print("Endi workspace ni tayorlimiza. Ungacha google proekt tayyorlab turadi. faylla orasida master.zip paydo bo'ldimi?")
+print("Ok. If you are logged in, click \033[4m'Create project'\033[0m or \033[4m'Создать проект'\033[0m here. Enter 'y' again.")
+wait("Look for a good deal. The script was found. If you find, enter 'y'")
+print("The menu open, and you type the project name there. Better write your web browser. For example, @intelekt_bot is an intel-bot, so there are fewer chaos")
+print("\033[1m It is a good that the name of the project is identical. Remember the name and id name of the project!\033[0m")
+print('Click on the project button. There is a "creating project..." or something like that')
+wait("Remember to rename the project name and id, or create or \033[95mсоздать\033[0m ni \033[95mbosing\033[0m")
+print("Now we need to run the workspace. Until then google produces a project. Has master.zip appeared in the file?")
 while raw_input("y/n:").lower() != "y":
-    print("Ozgina kuting va yana qarang. Paydo bo'lganda 'y' ni jo'nating.")
+    print("Wait a moment and look at it again. Enter 'y' when prompted.")
 
-print('Arxivdan chiqarilishi boshlanishi uchun nimadir yozing:')
+print('Type something to start removal from the archive:')
 raw_input(' ')
 
 try:
     os.system("unzip master.zip")
     data = open("./google_appengine/appcfg.py",'r').read()
     if "#!/usr/bin/env python" in data:
-        print("\033[4m Google app engine SDK o'rnatildi \033[0m")
+        print("\033[4m Google app engine SDK installed \033[0m")
     else:
-        print("\033[4m Google app engine SDK o'rnatilmadi. Fayllarni tozalang va qayta o'rnatishga harakat qiling.")
+        print("\033[4m Google app engine SDK not installed. Clean up and reinstall the files.")
 except:
-    print("\033[4m Google app engine SDK o'rnatilmadi. Fayllar hali tayyor emas")
-    
+    print("\033[4m Google app engine SDK not installed. Files are not ready yet")
+
 print("Ok, endi @botfather ga kiring va \033[95m/newbot\033[0m buyrug'ini bering. Keyin, bo'tni ismini (nikini) yozing. Undan keyin bo'tni \033[95m @userneym \033[0m ini yozing. Agar boy yasalmasa, unda boshqa userneym bilan harakat qiling. Bo't \033[95m @userneym`i \033[95m ohiri \033[95m bot \033[0m yoki \033[95m _bot \033[0m bilan tugashi kerak.")
 p = True
 API_TOKEN = ""
@@ -74,7 +74,7 @@ while admin_id == 0:
         admin_id = a_id
     except:
         print("\033[1m id raqam bo'ladi \033[0m")
-        
+
 
 
 print("Google app engine dagi ochgan projectingizni id sini yozing. Projectingizni nomini yozganingizda tegida chiqishi kerak. Ba'zida project nomi bilan bir xil bo'ladi, ba'zida esa, project nomi dan keyin chiziqcha va raqamlar bo'ladi")
@@ -85,7 +85,7 @@ while project_id == "":
     for ll in l:
         if ll in project_id:
             project_id = ''
-    
+
 
 print("Fayllar sozlanmoqda...")
 data = open('app_engine_installer/app_engine_project/app.yaml','r').read()
@@ -100,7 +100,7 @@ open('app_engine_installer/app_engine_project/main.py','w').write(data)
 print("Ok, ohiriga kelib qoldik. Bo't deyali tayyor. https://appengine.google.com saytiga kiring, yangi ochgan projectingizni tanlang. saytda tepada chap tomonda menyu bor. Menyuga kiring. Usha menyudan \033[95m APP ENGINE \033[0m ni tanlang.\n\nOchilgan stranitsada Choose language yoki Выбрать язык ni bosing. Pasda python ni belgisi chiqib keladi. Ushani tanlang. Karta chiqib kelganda Europe-West (Yevropa) ni tanlang. Pasda next ni bosing. Serverla tayyor bo'lishini kuting. tayyor bo'lganda esa, Tayyor dip yozing.")
 while raw_input('//>').lower() != "tayyor":
     print("Yaxshilab e'tibor berip qidiring. Script sizga buni qilib berolmaydi!")
-    
+
 print("Agar hammasi tayyor bo'lsa, bo'tni serverga joylimiza. faqat siz avtorizatsiyadan o'tishiz kere. Hozir link chiqadi va siz ucha linkga kirib kod ni copy qilib kelasiz. Keyin terminalga yozasiz. Ok?")
 raw_input('>')
 os.system('google_appengine/appcfg.py -A '+ project_id + " update app_engine_installer/app_engine_project/app.yaml --noauth_local_webserver")
